04ca090f4ae5f6b5ad4686685ba98584be177e0b
Stathis Dimitriadis <efstathios.dimitriadis@bbv.gr>
2022-05-16 17:00:11 +0300

    Replace ohmyposh with starship

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,43 +1,6 @@
-Import-Module -Name Terminal-Icons
-
-if ($host.Name -eq 'ConsoleHost')
-{
-    Import-Module PSReadLine
-}
-
-oh-my-posh --init --shell pwsh --config 'E:\Documents\PowerShell\ohmyposh.json' | Invoke-Expression
-
-# PowerShell parameter completion shim for the dotnet CLI
-Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
-    param($commandName, $wordToComplete, $cursorPosition)
-        dotnet complete --position $cursorPosition "$wordToComplete" | ForEach-Object {
-           [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
-        }
-}
-
-# Git parameter completion
-Import-Module posh-git
-
-# Replace git cmt with git commit
-Set-PSReadLineOption -CommandValidationHandler {
-    param([CommandAst]$CommandAst)
-
-    switch ($CommandAst.GetCommandName())
-    {
-        'git' {
-            $gitCmd = $CommandAst.CommandElements[1].Extent
-            switch ($gitCmd.Text)
-            {
-                'cmt' {
-                    [Microsoft.PowerShell.PSConsoleReadLine]::Replace(
-                        $gitCmd.StartOffset, $gitCmd.EndOffset - $gitCmd.StartOffset, 'commit')
-                }
-            }
-        }
-    }
-}
-
 # Predicitive Intellisense
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -PredictionViewStyle ListView
 Set-PSReadLineOption -EditMode Windows
+
+Invoke-Expression (&starship init powershell)
